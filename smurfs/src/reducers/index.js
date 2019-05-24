e32f9f3d7953233e7f1d28940a48f0e8bebb1f47
Ash <ashley.cruz.crespo@gmail.com>
2019-05-24 16:48:00 +0100

    working on reducers index

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -2,6 +2,10 @@
   Be sure to import in all of the action types from `../actions`
 */
 
+import { FETCH_SMURFS_START, FETCH_SMURFS_SUCCESS, ADD_SMURF_START, ADD_SMURF_SUCCESSFUL, 
+  DELETE_SMURF_START, DELETE_SMURF_SUCCESSFUL} from '../actions';
+
+
 /*
  Your initial/default state for this project could *Although does not have to* look a lot like this
  {
@@ -14,6 +18,14 @@
  }
 */
 
+const initialState = {
+  smurfs: [],
+   fetchingSmurfs: false,
+   error: "",
+   addingSmurf: false,
+   deletingSmurf: false
+}
+
 /*
   You'll only need one smurf reducer for this project.
   Feel free to export it as a default and import as rootReducer. 
@@ -21,3 +33,19 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+
+function reducer(state = initialState, action){
+  switch(action.type){
+    case FETCH_SMURFS_START:
+      return {
+        ...state,
+        error: '',
+        fetchingSmurfs: true
+      }
+    case FETCH_SMURFS_SUCCESS:
+      return {
+        ...state,
+        error: '',
+        fetchingSmurfs: false,
+        smurfs: action.payload
+      }
